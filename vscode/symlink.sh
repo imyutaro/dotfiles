72790340ceca2561ed974730462d3a4831958308
imyutaro <yutarofortech@gmail.com>
2019-09-19 20:09:11 +0900

    add symlink for vscode config

---
+++ vscode/symlink.sh
@@ -0,0 +1,6 @@
+#!bin/sh
+for dotfile in .?*
+do
+    ln -Fis "$PWD/keybindings.json" "$HOME/Library/Application Support/Code/User"
+    ln -Fis "$PWD/settings.json" "$HOME/Library/Application Support/Code/User"
+done
